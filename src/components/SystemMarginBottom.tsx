c3cb7d7ae5d2088ddc9a51405c66e67355d53f95
BORA <a52447879@gmail.com>
2024-10-23 21:43:45 +0900

    feat: 필터 설정 공통 컴포넌트

---
+++ src/components/SystemMarginBottom.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function SystemMarginBottom() {
+  return (
+    <div className="flex h-[30px] w-full items-end justify-center pb-2">
+      <svg
+        width="130"
+        height="5"
+        viewBox="0 0 130 5"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="130" height="5" rx="2" fill="#1A1A1A" />
+      </svg>
+    </div>
+  )
+}
