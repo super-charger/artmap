884d27a038f6381902cf28a3288e982b54737c5c
dave17code <dave17code@gmail.com>
2024-10-24 10:42:23 +0900

    feat: Home Now 상단 포스터 이미지 슬라이드 화면전환 적용 (Swiper 라이브러리 사용)

--- src/app/(has-navigation)/home/now/page.tsx
+++ src/app/(has-navigation)/home/now/page.tsx
@@ -4,6 +4,10 @@ import { useEffect, useState } from 'react'
 
 import Image from 'next/image'
 
+import 'swiper/css'
+import 'swiper/css/navigation'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
 import {
   getOngoingExhibitions,
   getUpcomingExhibitions,
@@ -22,9 +26,6 @@ interface Slide {
 
 export default function NowPage() {
   const [currentSlide, setCurrentSlide] = useState<number>(0)
-  const [indicatorPosition, setIndicatorPosition] = useState<'left' | 'right'>(
-    'left',
-  )
   const [exhibitionsData, setExhibitionsData] = useState<any[]>([])
 
   const slides: Slide[] = [
@@ -44,28 +45,16 @@ export default function NowPage() {
     },
   ]
 
-  const goToNextSlide = () => {
-    setCurrentSlide((prev) => (prev + 1) % slides.length)
-    setIndicatorPosition('right')
-  }
-
-  const goToPrevSlide = () => {
-    setCurrentSlide((prev) => (prev - 1 + slides.length) % slides.length)
-    setIndicatorPosition('left')
-  }
-
   useEffect(() => {
     async function fetchData() {
       try {
         // 진행 중인 전시 데이터를 가져오기
         const ongoingResponse = await getOngoingExhibitions()
         const ongoingExhibitions = ongoingResponse.data
-        console.log(ongoingExhibitions)
 
         // 곧 있을 전시 데이터를 가져오기
         const upcomingResponse = await getUpcomingExhibitions()
         const upcomingExhibitions = upcomingResponse.data
-        console.log(upcomingExhibitions)
 
         // 두 개의 전시 데이터를 각각 배열로 넣음
         setExhibitionsData([ongoingExhibitions, upcomingExhibitions])
@@ -78,52 +67,54 @@ export default function NowPage() {
 
   return (
     <div className="flex flex-col items-center">
+      {/* 슬라이드 섹션 및 ExhibitionCarousel을 동일한 부모 컨테이너에 넣음 */}
       <div className="relative mx-auto w-[100%] max-w-[1200px]">
         {/* 슬라이드 섹션 */}
         <div className="relative mb-[30px] flex h-[642px] items-center justify-center">
-          <div
-            className={`relative flex h-full w-full items-center justify-center ${slides[currentSlide].background}`}
-            onClick={(e) => {
-              const { left, width } = e.currentTarget.getBoundingClientRect()
-              const clickX = e.clientX - left
-              const center = width / 2
-              if (currentSlide === 0 && clickX > center) {
-                goToNextSlide()
-              } else if (currentSlide === 1 && clickX < center) {
-                goToPrevSlide()
-              }
-            }}
-          >
-            <div className="relative">
-              <Image
-                src={slides[currentSlide].imageSrc}
-                alt="image"
-                height={430}
-                width={300}
-              />
-              <div className="absolute bottom-0 left-0 h-1/3 w-full bg-gradient-to-t from-gray-800 to-transparent"></div>
-              <div className="absolute bottom-[10px] left-[10px] flex flex-col text-white">
-                <div className="mobile-extra-large font-bold">
-                  {slides[currentSlide].title}
-                </div>
-                <div className="mobile-text">{slides[currentSlide].date}</div>
-                <div className="mobile-title">{slides[currentSlide].place}</div>
-              </div>
-            </div>
-          </div>
-          {/* 슬라이드 인디케이터 */}
-          <div className="absolute bottom-[75px] left-1/2 h-[3px] w-[300px] -translate-x-1/2 transform rounded-full bg-gray-400">
+          {/* 상태바 */}
+          <div className="absolute bottom-[75px] left-1/2 z-50 h-[3px] w-[300px] -translate-x-1/2 transform rounded-full bg-gray-400">
             <div
               className="h-full rounded-full bg-white transition-all duration-500 ease-in-out"
               style={{
                 width: '50%',
                 transform:
-                  indicatorPosition === 'left' ? 'translateX(0)' : (
-                    'translateX(100%)'
-                  ),
+                  currentSlide === 0 ? 'translateX(0)' : 'translateX(100%)',
               }}
             ></div>
           </div>
+          {/* Swiper 슬라이더 */}
+          <Swiper
+            effect="fade"
+            onSlideChange={(swiper) => {
+              setCurrentSlide(swiper.activeIndex)
+            }}
+            className="h-full w-full"
+          >
+            {slides.map((slide, index) => (
+              <SwiperSlide key={index}>
+                <div
+                  className={`relative flex h-full w-full items-center justify-center ${slide.background}`}
+                >
+                  <div className="relative">
+                    <Image
+                      src={slide.imageSrc}
+                      alt="image"
+                      height={430}
+                      width={300}
+                    />
+                    <div className="absolute bottom-0 left-0 h-1/3 w-full bg-gradient-to-t from-gray-800 to-transparent"></div>
+                    <div className="absolute bottom-[10px] left-[10px] flex flex-col text-white">
+                      <div className="mobile-extra-large font-bold">
+                        {slide.title}
+                      </div>
+                      <div className="mobile-text">{slide.date}</div>
+                      <div className="mobile-title">{slide.place}</div>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
 
         {/* 진행 중인 전시 Carousel */}
