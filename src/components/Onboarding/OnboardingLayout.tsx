7ea79cb425fcbe5a1fe7b2e2c5d40106adfb92fd
KimPuro <puro@protonmail.ch>
2024-10-26 18:22:58 +0900

    refactor: 타입 코드 컨벤션 관련 수정

--- src/components/Onboarding/OnboardingLayout.tsx
+++ src/components/Onboarding/OnboardingLayout.tsx
@@ -1,7 +1,7 @@
-type onboardingLayoutProps = {
+type OnboardingLayoutProps = {
     children: React.ReactNode;
 }
-export default function OnboardingLayout({children}: onboardingLayoutProps) {
+export default function OnboardingLayout({children}: OnboardingLayoutProps) {
     return (
         <div className="flex justify-center items-center bg-primary_sub overflow-hidden w-full h-dvh relative">
             <main className="h-full w-[600px] bg-background_primary absolute">
