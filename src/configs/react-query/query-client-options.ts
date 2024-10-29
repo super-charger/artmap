8599adf1df7a856028a0c4bd5031eefd23ce43ff
BORA <a52447879@gmail.com>
2024-10-29 18:41:41 +0900

    fix: 리액트 쿼리 옵션 수정하기

--- src/configs/react-query/query-client-options.ts
+++ src/configs/react-query/query-client-options.ts
@@ -4,7 +4,7 @@ const queryClientOptions: QueryClientConfig = {
   defaultOptions: {
     queries: {
       /** 개발자 판단하에 설정해주세요 기본값은 0 입니다. */
-      staleTime: 0,
+      staleTime: 5 * 60 * 1000,
       /** 개발자 판단하에 설정해주세요 기본값은 3 입니다. */
       retry: 1,
     },
