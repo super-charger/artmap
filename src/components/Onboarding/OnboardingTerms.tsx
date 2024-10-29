27f41bf4b9808c8d3741b028ad93c89d6102c05b
KimPuro <puro@protonmail.ch>
2024-10-29 23:20:09 +0900

    refactor: useState map 형식으로 변경

--- src/components/Onboarding/OnboardingTerms.tsx
+++ src/components/Onboarding/OnboardingTerms.tsx
@@ -3,43 +3,48 @@ import {useState} from "react";
 import {useRouter} from "next/navigation";
 import Image from "next/image";
 
-export default function OnboardingTerms() {
+type CheckboxesState = {
+    isAllChecked: boolean;
+    isServiceChecked: boolean;
+    isPrivacyChecked: boolean;
+    isMarketingChecked: boolean;
+};
 
-    const [isAllChecked, setIsAllChecked] = useState(false);
-    const [isServiceChecked, setIsServiceChecked] = useState(false);
-    const [isPrivacyChecked, setIsPrivacyChecked] = useState(false);
-    const [isMarketingChecked, setIsMarketingChecked] = useState(false);
+export default function OnboardingTerms() {
+    const [checkboxes, setCheckboxes] = useState<CheckboxesState>({
+        isAllChecked: false,
+        isServiceChecked: false,
+        isPrivacyChecked: false,
+        isMarketingChecked: false,
+    });
     const router = useRouter();
 
     const handleAllCheckboxClick = () => {
-        const newCheckedState = !isAllChecked;
-        setIsAllChecked(newCheckedState);
-        setIsServiceChecked(newCheckedState);
-        setIsPrivacyChecked(newCheckedState);
-        setIsMarketingChecked(newCheckedState);
+        const newCheckedState = !checkboxes.isAllChecked;
+        setCheckboxes({
+            isAllChecked: newCheckedState,
+            isServiceChecked: newCheckedState,
+            isPrivacyChecked: newCheckedState,
+            isMarketingChecked: newCheckedState,
+        });
     };
 
-    const handleServiceCheckboxClick = () => {
-        setIsServiceChecked(!isServiceChecked);
-    };
-
-    const handlePrivacyCheckboxClick = () => {
-        setIsPrivacyChecked(!isPrivacyChecked);
-    };
-
-    const handleMarketingCheckboxClick = () => {
-        setIsMarketingChecked(!isMarketingChecked);
+    const handleCheckboxClick = (key: keyof CheckboxesState) => {
+        setCheckboxes(prevState => ({
+            ...prevState,
+            [key]: !prevState[key],
+        }));
     };
 
     const handleSubmit = () => {
         router.push('/onboarding/analyze');
     };
-    const isButtonEnabled = isServiceChecked && isPrivacyChecked;
+
+    const isButtonEnabled = checkboxes.isServiceChecked && checkboxes.isPrivacyChecked;
 
     return (
         <>
-            <div
-                className="left-[16px] relative flex-col justify-start items-start gap-10 inline-flex w-full mt-16">
+            <div className="left-[16px] relative flex-col justify-start items-start gap-10 inline-flex w-full mt-16">
                 <div className="self-stretch flex-col justify-center items-start gap-5 flex w-11/12">
                     <div className="self-stretch flex-col justify-end items-start gap-2 flex w-full">
                         <div className="self-stretch justify-between items-center inline-flex w-full border-b-2 border-black pb-2">
@@ -48,8 +53,8 @@ export default function OnboardingTerms() {
                             </div>
                             <div className="w-6 h-6 relative cursor-pointer" onClick={handleAllCheckboxClick}>
                                 <Image
-                                    src={isAllChecked ? '/icons/check-box/circle-check-on.svg' : '/icons/check-box/circle-check-off.svg'}
-                                    alt={isAllChecked ? "circle-check-on" : "circle-check-off"}
+                                    src={checkboxes.isAllChecked ? '/icons/check-box/circle-check-on.svg' : '/icons/check-box/circle-check-off.svg'}
+                                    alt={checkboxes.isAllChecked ? "circle-check-on" : "circle-check-off"}
                                     className="absolute right-0"
                                     width={24}
                                     height={24}
@@ -60,16 +65,15 @@ export default function OnboardingTerms() {
                     <div className="self-stretch flex-col justify-center items-center flex w-full">
                         <div className="self-stretch py-[13px] justify-between items-center inline-flex w-full">
                             <div>
-                    <span className="text-[#808080] text-xs font-normal font-['Noto Sans KR'] underline leading-[18px]">
-                        서비스 이용을 위한 필수약관 동의
-                    </span>
-                                <span
-                                    className="text-[#7f20f7] text-xs font-normal font-['Noto Sans KR'] leading-[18px]"> (필수)</span>
+                                <span className="text-[#808080] text-xs font-normal font-['Noto Sans KR'] underline leading-[18px]">
+                                    서비스 이용을 위한 필수약관 동의
+                                </span>
+                                <span className="text-[#7f20f7] text-xs font-normal font-['Noto Sans KR'] leading-[18px]"> (필수)</span>
                             </div>
-                            <div className="w-6 h-6 relative cursor-pointer" onClick={handleServiceCheckboxClick}>
+                            <div className="w-6 h-6 relative cursor-pointer" onClick={() => handleCheckboxClick('isServiceChecked')}>
                                 <Image
-                                    src={isServiceChecked ? '/icons/check-box/check-line-on.svg' : '/icons/check-box/check-line-off.svg'}
-                                    alt={isServiceChecked ? "check-line-on" : "check-line-off"}
+                                    src={checkboxes.isServiceChecked ? '/icons/check-box/check-line-on.svg' : '/icons/check-box/check-line-off.svg'}
+                                    alt={checkboxes.isServiceChecked ? "check-line-on" : "check-line-off"}
                                     width={24}
                                     height={24}
                                 />
@@ -77,30 +81,28 @@ export default function OnboardingTerms() {
                         </div>
                         <div className="self-stretch py-[13px] justify-between items-center inline-flex w-full">
                             <div>
-                    <span className="text-[#808080] text-xs font-normal font-['Noto Sans KR'] underline leading-[18px]">
-                        개인정보수집 및 이용 동의
-                    </span>
-                                <span
-                                    className="text-[#7f20f7] text-xs font-normal font-['Noto Sans KR'] leading-[18px]"> (필수)</span>
+                                <span className="text-[#808080] text-xs font-normal font-['Noto Sans KR'] underline leading-[18px]">
+                                    개인정보수집 및 이용 동의
+                                </span>
+                                <span className="text-[#7f20f7] text-xs font-normal font-['Noto Sans KR'] leading-[18px]"> (필수)</span>
                             </div>
-                            <div className="w-6 h-6 relative cursor-pointer" onClick={handlePrivacyCheckboxClick}>
+                            <div className="w-6 h-6 relative cursor-pointer" onClick={() => handleCheckboxClick('isPrivacyChecked')}>
                                 <Image
-                                    src={isPrivacyChecked ? '/icons/check-box/check-line-on.svg' : '/icons/check-box/check-line-off.svg'}
-                                    alt={isPrivacyChecked ? "check-line-on" : "check-line-off"}
+                                    src={checkboxes.isPrivacyChecked ? '/icons/check-box/check-line-on.svg' : '/icons/check-box/check-line-off.svg'}
+                                    alt={checkboxes.isPrivacyChecked ? "check-line-on" : "check-line-off"}
                                     width={24}
                                     height={24}
                                 />
                             </div>
                         </div>
                         <div className="self-stretch py-[13px] justify-between items-center inline-flex w-full">
-                            <div
-                                className="text-[#808080] text-xs font-normal font-['Noto Sans KR'] underline leading-[18px]">
+                            <div className="text-[#808080] text-xs font-normal font-['Noto Sans KR'] underline leading-[18px]">
                                 마케팅 수신 동의
                             </div>
-                            <div className="w-6 h-6 relative cursor-pointer" onClick={handleMarketingCheckboxClick}>
+                            <div className="w-6 h-6 relative cursor-pointer" onClick={() => handleCheckboxClick('isMarketingChecked')}>
                                 <Image
-                                    src={isMarketingChecked ? '/icons/check-box/check-line-on.svg' : '/icons/check-box/check-line-off.svg'}
-                                    alt={isMarketingChecked ? "check-line-on" : "check-line-off"}
+                                    src={checkboxes.isMarketingChecked ? '/icons/check-box/check-line-on.svg' : '/icons/check-box/check-line-off.svg'}
+                                    alt={checkboxes.isMarketingChecked ? "check-line-on" : "check-line-off"}
                                     width={24}
                                     height={24}
                                 />
