f92bb41b2f2fe32a2b193c92d043b81aa0993026
KimPuro <puro@protonmail.ch>
2024-10-29 23:14:19 +0900

    feat: AnalyzeFooter 컴포넌트 추가

--- src/app/onboarding/analyze2/page.tsx
+++ src/app/onboarding/analyze2/page.tsx
@@ -1,6 +1,11 @@
+import OnboardingHeader from "@/components/Onboarding/OnboardingHeader";
+import AnalyzeFooter from "@/components/Onboarding/AnalyzeFooter";
+
 export default function Page() {
     return (
         <>
+            <OnboardingHeader backButton={true}/>
+            <AnalyzeFooter/>
 
         </>
     )
