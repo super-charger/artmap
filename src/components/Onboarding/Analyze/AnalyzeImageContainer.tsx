c3f02f14953d164f90586f875f084df1dcb69c28
KimPuro <puro@protonmail.ch>
2024-10-30 04:41:23 +0900

    feat: 취향 분석 컴포넌트의 선택한 이미지 상태 저장

--- src/components/Onboarding/Analyze/AnalyzeImageContainer.tsx
+++ src/components/Onboarding/Analyze/AnalyzeImageContainer.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 
 export default function AnalyzeImageContainer() {
     const [imageData, setImageData] = useState<any>();
+    const [selectedImageIds, setSelectedImageIds] = useState<number[]>([]);
 
     useEffect(() => {
         async function fetchData() {
@@ -14,12 +15,27 @@ export default function AnalyzeImageContainer() {
         }
         fetchData();
     }, []);
-if(!imageData) return null;
+
+    const toggleImageSelection = (id: number) => {
+        setSelectedImageIds(prevSelectedImageIds =>
+            prevSelectedImageIds.includes(id)
+                ? prevSelectedImageIds.filter(imageId => imageId !== id)
+                : [...prevSelectedImageIds, id]
+        );
+        console.log(selectedImageIds);
+    };
+
+    if (!imageData) return null;
+
     return (
         <div className="overflow-auto h-full grid grid-cols-2 gap-6">
-            {imageData.data.map((image: any,) => (
-                <div key={image.id} className='relative w-[250px] h-[250px]'>
-                    <Image src={image.thumbnail} alt={`image-${image.id}`} fill={true}/>
+            {imageData.data.map((image: any) => (
+                <div
+                    key={image.id}
+                    className={`relative w-[250px] h-[250px] ${selectedImageIds.includes(image.id) ? 'border-4 border-purple-500' : ''}`}
+                    onClick={() => toggleImageSelection(image.id)}
+                >
+                    <Image src={image.thumbnail} alt={`image-${image.id}`} fill={true} />
                 </div>
             ))}
         </div>
