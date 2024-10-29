ce0bd0182dd0d6ebd7c38077466044c3ca0847cb
KimPuro <puro@protonmail.ch>
2024-10-30 05:11:57 +0900

    style: 온보딩 타이틀의 폰트 크기 변경

--- src/app/onboarding/analyze4/page.tsx
+++ src/app/onboarding/analyze4/page.tsx
@@ -2,11 +2,14 @@
 import OnboardingHeader from "@/components/Onboarding/OnboardingHeader";
 import AnalyzeFooter from "@/components/Onboarding/Analyze/AnalyzeFooter";
 import OnboardingTitle from "@/components/Onboarding/OnboardingTitle";
-import AnalyzeContainer from "@/components/Onboarding/Analyze/AnalyzeContainer";
-import AnalyzeImageContainer from "@/components/Onboarding/Analyze/AnalyzeImageContainer";
+
 import {useRouter} from "next/navigation";
+import GenreButton from "@/components/Onboarding/Analyze/GenreButton";
+import GenreContainer from "@/components/Onboarding/Analyze/GenreContainer";
+import {COLOR} from "@/constants/analyze";
 
 export default function Page() {
+
     const router = useRouter();
     const buttonClick = () => {
         router.push('/home/now')
@@ -14,11 +17,15 @@ export default function Page() {
     return (
         <>
             <OnboardingHeader backButton={true}/>
-            <OnboardingTitle>마음에 드는 작품을<br/>골라주세요.</OnboardingTitle>
-            <AnalyzeContainer>
-                <AnalyzeImageContainer/>
-            </AnalyzeContainer>
-            <AnalyzeFooter buttonText={'다음'} nextButton={'취향분석 다음에 하기'} onClick={buttonClick}/>
+            <OnboardingTitle>내가 관심있는 색상을<br/>전부 골라주세요.</OnboardingTitle>
+            <GenreContainer>
+                {COLOR.map((color,index) => (
+                    <GenreButton key={color[index]}>
+                        {color}
+                    </GenreButton>
+                ))}
+            </GenreContainer>
+            <AnalyzeFooter buttonText={'취향분석 완료'} nextButton={'취향분석 다음에 하기'} onClick={buttonClick}/>
 
 
         </>
