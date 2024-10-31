c2ed0b70c559ed2f96880fe70a504149fad3f9d1
KimPuro <puro@protonmail.ch>
2024-10-31 22:01:48 +0900

    fix: overlay 타입 수정

--- src/app/(has-navigation)/exhibitions/location/_source/hooks/useVisibleElements.tsx
+++ src/app/(has-navigation)/exhibitions/location/_source/hooks/useVisibleElements.tsx
@@ -47,7 +47,7 @@ export const useVisibleElements = () => {
     const filteredMarkers = !isOverlayZoom ? visibleExhibitions : []
     const filteredOverlays =
       isOverlayZoom ?
-        data.areaGroups.filter((overlay:OverlayApiType) =>
+        data.areaGroups.filter((overlay: any) =>
           currentBounds.contain(
             new kakao.maps.LatLng(overlay.position.lat, overlay.position.lng),
           ),
