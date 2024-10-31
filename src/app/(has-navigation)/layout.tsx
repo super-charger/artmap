4e2f234ca596fdddc48468d336f61a8c467b453b
KimPuro <puro@protonmail.ch>
2024-10-31 19:31:43 +0900

    fix: 빌드시 오류 발생하는 useSearchParams를 suspense로 래핑

--- src/app/(has-navigation)/layout.tsx
+++ src/app/(has-navigation)/layout.tsx
@@ -1,12 +1,14 @@
-import { ReactNode } from 'react'
+import {ReactNode, Suspense} from 'react'
 
 import BottomNavigation from '@/components/BottomNavigation'
 
 export default function MainLayout({ children }: { children: ReactNode }) {
   return (
     <>
+        <Suspense fallback={<div>Loading...</div>}>  // update: 임시 Suspense 추가
       {children}
       <BottomNavigation />
+        </Suspense>
     </>
   )
 }
